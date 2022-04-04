9f5a3cbeef444460326560677472af08761c1fcf
Arellano-Jann <jannrellano@nevada.unr.edu>
2022-04-03 22:42:37 -0700

    Finished stubbing

--- src/BinarySearchTree.cpp
+++ src/BinarySearchTree.cpp
@@ -8,12 +8,88 @@ bool BinarySearchTree<T>::add(const T& newEntry){
 
 }
 
+template <typename T>
+bool BinarySearchTree<T>::remove(const T& anEntry){
 
+}
+
+template <typename T>
+void BinarySearchTree<T>::clear(){
+
+}
+
+template <typename T>
+int BinarySearchTree<T>::getHeight() const{
+
+}
+
+template <typename T>
+int BinarySearchTree<T>::getNumberOfNodes() const{
+
+}
+
+template <typename T>
+T BinarySearchTree<T>::getRootData() const{
+
+}
+
+template <typename T>
+T BinarySearchTree<T>::getEntry(const T& anEntry) const{
+
+}
+
+template <typename T>
+bool BinarySearchTree<T>::contains(const T& anEntry) const{
+
+}
+
+template <typename T>
+bool BinarySearchTree<T>::isEmpty() const{
+
+}
+
+template <typename T>
+void BinarySearchTree<T>::preorderTraverse(void visit(T&)) const{
+
+}
+
+template <typename T>
+void BinarySearchTree<T>::inorderTraverse(void visit(T&)) const{
+
+}
+
+template <typename T>
+void BinarySearchTree<T>::postorderTraverse(void visit(T&)) const{
+
+}
+
+template <typename T>
+BinarySearchTree<T>::~BinarySearchTree(){ clear(); }
 
 
 
 
 template <typename T>
 BinaryNode<T>* placeNode(BinaryNode<T>* subTreePtr, BinaryNode<T>* newNodePtr){
+
+}
+
+template <typename T>
+BinaryNode<T>* removeValue(BinaryNode<T>* subTreePtr, const T target, bool& success){
+
+}
+
+template <typename T>
+BinaryNode<T>* removeNode(BinaryNode<T>* nodePtr){
+
+}
+
+template <typename T>
+BinaryNode<T>* removeLeftmostNode(BinaryNode<T>* nodePtr, T& inorderSuccessor){
+
+}
+
+template <typename T>
+BinaryNode<T>* findNode(BinaryNode<T>* nodePtr, const T& target) const{
 
 }
