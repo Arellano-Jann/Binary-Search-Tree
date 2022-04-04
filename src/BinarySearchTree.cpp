a91edf0873d2dc7ad4f64b03be25b9c2ac204261
Arellano-Jann <jannrellano@nevada.unr.edu>
2022-04-03 20:48:20 -0700

    Started stubbing

---
+++ src/BinarySearchTree.cpp
@@ -0,0 +1,19 @@
+template <typename T>
+BinarySearchTree<T>::BinarySearchTree()
+    : rootPtr(nullptr)
+{}
+
+template <typename T>
+bool BinarySearchTree<T>::add(const T& newEntry){
+
+}
+
+
+
+
+
+
+template <typename T>
+BinaryNode<T>* placeNode(BinaryNode<T>* subTreePtr, BinaryNode<T>* newNodePtr){
+
+}
