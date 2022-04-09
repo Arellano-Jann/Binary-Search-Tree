e9f1091f204306436c48b5a701283ddddcc79e27
Arellano-Jann <jannrellano@nevada.unr.edu>
2022-04-09 16:14:45 -0700

    Added main functionality

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,9 @@ int main(){
     for (int i = 0; i < 100; i++){
         tree.add(gen.getArray()[i]);
     }
+    tree.add(1);
+    std::cout << "Tree height: " << tree.getHeight() << std::endl;
+    std::cout << "Tree size: " << tree.getNumberOfNodes() << std::endl;
     tree.preorderTraverse( [] (int& i){ std::cout << i << " "; } );
     return 0;
 }
