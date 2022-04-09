ece003f9e650a856f5d3b7bdd41c859aced253c1
Arellano-Jann <jannrellano@nevada.unr.edu>
2022-04-09 00:03:10 -0700

    Finished protected comments

--- src/BinarySearchTree.cpp
+++ src/BinarySearchTree.cpp
@@ -126,26 +126,52 @@ BinaryNode<T>* BinarySearchTree<T>::placeNode(BinaryNode<T>* subTreeRootPtr, Bin
     return subTreePtr;
 }
 
-// Recursive algorithm to remove node from tree
+
+/**
+ * @brief Recursively searches for the target in the subTree and removes it if found. This is a helper method to remove.
+ * @details 1. Returns false if the tree is empty or target is not in subTree. \n
+ *         2. Checks if subTreeRootPtr is greater than the target. \n
+ *        3. If so, recursively searches the left subtree with the removeValue function. \n
+ *       4. If not, recursively searches the right subtree with the removeValue function. \n
+ *     5. If the target is found, the node is removed with the removeNode function and the function returns true. \n
+ * 
+ * @tparam T The type of data in the tree.
+ * @param subTreeRootPtr The current root of the subTree.
+ * @param target The target to delete.
+ * @param success Returns true if found and deleted.
+ * @return BinaryNode<T>* Returns the filled subtree of the deleted node.
+ */
 template <typename T>
 BinaryNode<T>* BinarySearchTree<T>::removeValue(BinaryNode<T>* subTreeRootPtr, const T target, bool& success){
-    if(subTreeRootPtr == nullptr){ // returns false if tree is empty
+    if(subTreeRootPtr == nullptr){ // Step 1. // returns false if tree is empty
         success = false;
     }
-    else if(subTreeRootPtr->getItem() == target){ // removes node if subTreeRootPtr is the target
+    else if(subTreeRootPtr->getItem() == target){ // Step 5. // removes node if subTreeRootPtr is the target
         subTreeRootPtr = removeNode(subTreeRootPtr); // what if removal node has a child?
         success = true;
     }
-    else if(subTreeRootPtr->getItem() > target){ // target has to be in the left side of the tree if it is less than subTreeRootPtr
+    else if(subTreeRootPtr->getItem() > target){ // Step 3. // target has to be in the left side of the tree if it is less than subTreeRootPtr
         subTreeRootPtr->setLeftChildPtr(removeValue(subTreeRootPtr->getLeftChildPtr(), target, success)); // recursively sets the left child of subTreeRootPtr to the target cascadingly with the removeNode method called at the end
     }
-    else{ // target has to be in the right side of the tree if it is greater than subTreeRootPtr
+    else{ // Step 4. // target has to be in the right side of the tree if it is greater than subTreeRootPtr
         subTreeRootPtr->setRightChildPtr(removeValue(subTreeRootPtr->getRightChildPtr(), target, success)); // recursively sets the right child of subTreeRootPtr to the target cascadingly with the removeNode method called at the end
     }
     return subTreeRootPtr;
 }
 
-// helper method to removeValue. connects the children of the node to be removed to the upper tree
+
+/**
+ * @brief Removes nodePtr and returns the children of nodePtr. It returns this value to connect to the upper tree. This is a helper method to removeValue.
+ * @details 1. If nodePtr is a leaf, there is nothing to connect. \n
+ *          2. If nodePtr has a right child, the right child is returned. \n
+ *          3. If nodePtr has a left child, the left child is returned. \n
+ *         4. If nodePtr has both children, the smallest value in the right subTree of nodePtr is found. Replace the "to be deleted" node's data with the smallest value. Remove the smallest value found in the right subtree since this is now a duplicate. \n
+ *          5. This workes because the smallest value in a right subtree should have no left nodes. The only possible nodes for a smallest value in a subtree is right nodes. \n
+ * 
+ * @tparam T The type of data in the tree.
+ * @param nodePtr The node to be deleted.
+ * @return BinaryNode<T>* Returns the filled subtree of the deleted node for further connections to the parent.
+ */
 template <typename T>
 BinaryNode<T>* BinarySearchTree<T>::removeNode(BinaryNode<T>* nodePtr){
     BinaryNode<T>* nodeToConnectPtr;
@@ -173,7 +199,7 @@ BinaryNode<T>* BinarySearchTree<T>::removeNode(BinaryNode<T>* nodePtr){
 }
 
 /**
- * @brief Finds the least value in the tree using subTreeRootPtr as the root. It then deletes the least value while keeping the structure in tact.
+ * @brief Finds the least value in the tree using subTreeRootPtr as the root. It then deletes the least value while keeping the structure in tact. This is a helper method to removeNode.
  * @details 1. If subTreeRootPtr has a left child, execute recursively until the least value is found. \n
  *        1b. Set as subTreeRootPtr \n 
  *        2. Set the inorderSuccessor to be the least value. \n
@@ -197,6 +223,19 @@ BinaryNode<T>* BinarySearchTree<T>::removeLeftmostNode(BinaryNode<T>* subTreeRoo
     return subTreeRootPtr;
 }
 
+/**
+ * @brief Recursively finds the node in nodePtr with the data value of target.
+ * @details 1. If nodePtr is nullptr, return nullptr for not found. \n
+ *          2. If nodePtr is the target, return nodePtr. \n
+ *          3. If nodePtr is greater than target, execute recursively on the left child. This is the left child because of the equality system of a binary tree. \n
+ *          4. If nodePtr is less than target, execute recursively on the right child. \n
+ *          5. Repeat recursively until the target is found.
+ * 
+ * @tparam T The type of the data in the tree.
+ * @param nodePtr 
+ * @param target Data value to be found.
+ * @return BinaryNode<T>* 
+ */
 template <typename T>
 BinaryNode<T>* BinarySearchTree<T>::findNode(BinaryNode<T>* nodePtr, const T& target) const{
     if(nodePtr == nullptr){
@@ -205,7 +244,7 @@ BinaryNode<T>* BinarySearchTree<T>::findNode(BinaryNode<T>* nodePtr, const T& ta
     else if(nodePtr->getItem() == target){
         return nodePtr;
     }
-    else if(target < nodePtr->getItem()){
+    else if(nodePtr->getItem() > target){
         return findNode(nodePtr->getLeftChildPtr(), target);
     }
     else{
