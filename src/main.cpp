af89fa98242ee281fce0c806d01226ecca857cdf
Arellano-Jann <jannrellano@nevada.unr.edu>
2022-04-09 15:58:05 -0700

    Added main code

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,13 @@
 #include <iostream>
-#include 
 #include "../headers/BinarySearchTree.h"
+#include "../headers/NumberGenerator.h"
 
 int main(){
     BinarySearchTree<int> tree;
-
-    tree.add(5);
-    tree.add(3);
-    tree.add(10);
-    tree.add(2);
-    tree.add(4);
-    tree.add(8);
-    tree.add(9);
-    tree.add(11);
+    NumberGenerator gen(100);
+    for (int i = 0; i < 100; i++){
+        tree.add(gen.getArray()[i]);
+    }
+    tree.preorderTraverse(std::cout);
     return 0;
 }
